8e598c2db05dceecc4333edb239dc90a3f27aacf
Aleksandar Petkov <iskander9207@icloud.com>
2024-02-03 22:48:19 +0200

    fixed some styles and added anchor tags for the links

--- top-movie/src/components/Header/Header.jsx
+++ top-movie/src/components/Header/Header.jsx
@@ -8,9 +8,15 @@ function Header() {
                 <div className="logo">top movie</div>
                 <div className="main-nav">
                     <ul className="nav-list">
-                        <li className="nav-list-item">Home</li>
-                        <li className="nav-list-item">Movies</li>
-                        <li className="nav-list-item">Actors</li>
+                        <li className="nav-list-item">
+                            <a href="#">Home</a>
+                        </li>
+                        <li className="nav-list-item">
+                            <a href="#">Movies</a>
+                        </li>
+                        <li className="nav-list-item">
+                            <a href="#">Actors</a>
+                        </li>
                     </ul>
                 </div>
             </div>
