9444d3a136b884a1982f9b083f94fba21131850f
Aleksandar Petkov <iskander9207@icloud.com>
2024-02-05 18:38:44 +0200

    configured react-router-dom

--- top-movie/src/App.jsx
+++ top-movie/src/App.jsx
@@ -1,15 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import AboutUs from "./components/AboutUs/AboutUs";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
-        <>
+        <Router>
             <Header />
-            <AboutUs />
+
+            <Routes>
+                <Route path="/" element={<AboutUs />} />
+            </Routes>
             <Footer />
-        </>
+        </Router>
     );
 }
 
